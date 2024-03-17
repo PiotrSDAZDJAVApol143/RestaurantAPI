b6cafa117ce78578e8016504ecd0b82a882f433f
PiotrSDAZDJAVApol143 <xav88er@gmail.com>
2024-03-17 12:47:21 +0100

    RestaurantTableController - update All tables in Restaurant By ID - is working

--- src/main/java/com/example/restaurantapi/service/RestaurantTableService.java
+++ src/main/java/com/example/restaurantapi/service/RestaurantTableService.java
@@ -1,5 +1,6 @@
 package com.example.restaurantapi.service;
 
+import com.example.restaurantapi.dto.RestaurantTableDTO;
 import com.example.restaurantapi.model.Restaurant;
 import com.example.restaurantapi.model.RestaurantTable;
 import com.example.restaurantapi.repository.RestaurantRepository;
@@ -8,6 +9,8 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,15 +26,7 @@ public class RestaurantTableService {
         return restaurantTableRepository.save(restaurantTable);
     }
 
-    public RestaurantTable updateCapacityOfTableById(Long restaurantId, Long tableId, Integer capacityOfTable) {
-        RestaurantTable restaurantTable = restaurantTableRepository.findById(tableId)
-                .orElseThrow(() -> new EntityNotFoundException("Table with id " + tableId + " not found"));
-        if (!restaurantTable.getRestaurant().getId().equals(restaurantId)) {
-            throw new IllegalArgumentException("Table with id " + tableId + " does not belong to restaurant with id " + restaurantId);
-        }
-        restaurantTable.setCapacityOfTable(capacityOfTable);
-        return restaurantTableRepository.save(restaurantTable);
-    }
+
 
     public void deleteRestaurantTable(Long restaurantId, Long tableId) {
         RestaurantTable restaurantTable = restaurantTableRepository.findById(tableId)
@@ -41,4 +36,27 @@ public class RestaurantTableService {
         }
         restaurantTableRepository.deleteById(tableId);
     }
+
+    public List<RestaurantTable> updateTablesInRestaurant(Long restaurantId, List<RestaurantTableDTO> tables) {
+        Restaurant restaurant = restaurantService.findRestaurantTableById(restaurantId);
+        List<RestaurantTable> updatedTables = new ArrayList<>();
+        for (RestaurantTableDTO tableDTO : tables) {
+            RestaurantTable table = restaurantTableRepository.findById(tableDTO.getId()).orElse(new RestaurantTable());
+            table.setRestaurant(restaurant);
+            table.setTableNumber(tableDTO.getTableNumber());
+            table.setCapacityOfTable(tableDTO.getCapacityPerTable());
+            table.setReservationCheck(tableDTO.getReservationCheck());
+            updatedTables.add(restaurantTableRepository.save(table));
+        }
+        return updatedTables;
+    }
 }
+//   public RestaurantTable updateCapacityOfTableById(Long restaurantId, Long tableId, Integer capacityOfTable) {
+//       RestaurantTable restaurantTable = restaurantTableRepository.findById(tableId)
+//               .orElseThrow(() -> new EntityNotFoundException("Table with id " + tableId + " not found"));
+//       if (!restaurantTable.getRestaurant().getId().equals(restaurantId)) {
+//           throw new IllegalArgumentException("Table with id " + tableId + " does not belong to restaurant with id " + restaurantId);
+//       }
+//       restaurantTable.setCapacityOfTable(capacityOfTable);
+//       return restaurantTableRepository.save(restaurantTable);
+//   }
